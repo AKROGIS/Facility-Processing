c5d97238941052bd08d6e9c8c9e51b8a34cd67a2
Regan Sarwas <regan_sarwas@nps.gov>
2015-07-21 11:59:59 -0800

    Update Make Thumbnails
    use folder structure not CSV

--- Photo Processing/make_thumbnails.py
+++ Photo Processing/make_thumbnails.py
@@ -6,40 +6,70 @@ __author__ = "Regan Sarwas, GIS Team, Alaska Region, National Park Service"
 __email__ = "regan_sarwas@nps.gov"
 __copyright__ = "Public Domain - product of the US Government"
 
-from PIL import Image
+
+import sys
 import os
-import os.path
-import csv
 
-import apply_orientation
+# dependency PIL (now maintained as Pillow)
+# C:\Python27\ArcGIS10.3\Scripts\pip.exe install Pillow
 
-root = os.path.dirname(os.path.abspath(__file__))
-photo_csv = os.path.join(root, "PhotoList.csv")
-thumbdir = os.path.join(root, "thumb")
+try:
+    from PIL import Image
+except ImportError:
+    Image = None
+    print 'PIL module not found, make sure it is installed with'
+    print 'C:\Python27\ArcGIS10.3\Scripts\pip.exe install Pillow'
+    sys.exit()
 
-size = (200, 150)
+import apply_orientation  # dependency on PIL
 
-if not os.path.exists(thumbdir):
-    os.mkdir(thumbdir)
 
-with open(photo_csv, 'rb') as fh:
-    fh.readline()  # remove the header
-    photo_reader = csv.reader(fh, delimiter=',', quotechar='"')
-    for row in photo_reader:
-        # folder,photo,id,namedate,exifdate,lat,lon,gpsdate,size,filedate
-        # 0     ,1    ,2 ,3       ,4       ,5  ,6  ,7      ,8   ,9
-        unit = row[0]
-        name = row[1]
-        src = os.path.join(root, unit, name)
-        dest_dir = os.path.join(thumbdir, unit)
-        if not os.path.exists(dest_dir):
-            os.mkdir(dest_dir)
-        dest = os.path.join(dest_dir, name)
-        if os.path.exists(src) and (not os.path.exists(dest) or os.path.getmtime(dest) < os.path.getmtime(dest)):
-            try:
-                im = Image.open(src)
-                im = apply_orientation.apply_orientation(im)
-                im.thumbnail(size, Image.ANTIALIAS)
-                im.save(dest)
-            except IOError:
-                print "cannot create thumbnail for", src
+def is_jpeg(path):
+    if not os.path.isfile(path):
+        return False
+    ext = os.path.splitext(path)[1].lower()
+    return ext in ['.jpg', '.jpeg']
+
+
+def parks(parent):
+    return [f for f in os.listdir(parent) if os.path.isdir(os.path.join(parent, f))]
+
+
+def photos(parkdir):
+    return [f for f in os.listdir(parkdir) if is_jpeg(os.path.join(parkdir, f))]
+
+
+def make_thumbs(base, size):
+    origdir = os.path.join(base, "ORIGINAL")
+    thumbdir = os.path.join(base,  "THUMB")
+
+    if not os.path.exists(origdir):
+        print "Photo directory: " + origdir + " does not exit."
+        return
+
+    if not os.path.exists(thumbdir):
+        os.mkdir(thumbdir)
+
+    for park in parks(origdir):
+        orig_park_path = os.path.join(origdir, park)
+        new_park_path = os.path.join(thumbdir, park)
+        if not os.path.exists(new_park_path):
+            os.mkdir(new_park_path)
+        for photo in photos(orig_park_path):
+            src = os.path.join(orig_park_path, photo)
+            dest = os.path.join(new_park_path, photo)
+            if os.path.exists(src) and (not os.path.exists(dest) or os.path.getmtime(dest) < os.path.getmtime(dest)):
+                try:
+                    im = Image.open(src)
+                    im = apply_orientation.apply_orientation(im)
+                    im.thumbnail(size, Image.ANTIALIAS)
+                    im.save(dest)
+                except IOError:
+                    print "Cannot create thumbnail for", src
+
+
+if __name__ == '__main__':
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    base_dir = os.path.dirname(script_dir)
+    thumb_size = (200, 150)
+    make_thumbs(base_dir, thumb_size)
