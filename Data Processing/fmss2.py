30ca8ec2888120d35dd1e29e1bba94116ac96d97
Regan Sarwas <regan_sarwas@nps.gov>
2016-07-27 15:42:50 -0800

    Add CSV output from XML response

--- Data Processing/fmss2.py
+++ Data Processing/fmss2.py
@@ -4,6 +4,8 @@
 # This no longer works, as of July 27, 2016, because it is requiring authentication
 
 import urllib2
+import xml.etree.ElementTree as eT
+import csv
 
 assetCode = '1100'
 siteid = 'P417'
@@ -11,6 +13,7 @@ siteid = 'P417'
 host = "inp2420maxsys1u:9082"
 action = '"urn:processDocument"'
 endpoint = r"http://inp2420maxsys1u:9082/meaweb/services/FMSSGISLOCQ"
+out_file = 'out.csv'
 
 query = u"""<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
   <soap:Body>
@@ -36,4 +39,14 @@ request = urllib2.Request(url = endpoint,
                           headers = headers,
                           data = encoded_query)
 response = urllib2.urlopen(request).read()
-print response
+#print response
+
+xml_root = eT.fromstring(response)
+locations = xml_root.iter('LOCATIONS')
+tags = ['LOCATION', 'DESCRIPTION', 'LO11', 'LO14', 'LO2', 'LO5', 'LO6', 'LO7', 'LO9', 'STATUS', 'YEARBUILT']
+with open(out_file, 'wb') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(tags)
+    for location in locations:
+        data = [location.find(tag).text for tag in tags]
+        csv_writer.writerow(data)
