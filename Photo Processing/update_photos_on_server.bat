98c94c3822a2a9fde7e0ba1b30918a910f8ad5a4
Regan Sarwas <regan_sarwas@nps.gov>
2016-11-10 11:32:35 -0900

    powershell does not grok cp command

--- Photo Processing/update_photos_on_server.bat
+++ Photo Processing/update_photos_on_server.bat
@@ -3,6 +3,6 @@ cls
 REM - Photos to the webserver
 T:\PROJECTS\AKR\FMSS\PHOTOS\PROCESSING\cmd\robocopy.exe T:\PROJECTS\AKR\FMSS\PHOTOS\WEB \\akrgis.nps.gov\inetApps\fmss\photos\web /R:5 /W:5 /Z /MIR /LOG:T:\PROJECTS\AKR\FMSS\PHOTOS\PROCESSING\cmd\photoweblogfile.txt 
 T:\PROJECTS\AKR\FMSS\PHOTOS\PROCESSING\cmd\robocopy.exe T:\PROJECTS\AKR\FMSS\PHOTOS\THUMB \\akrgis.nps.gov\inetApps\fmss\photos\thumb /R:5 /W:5 /Z /MIR /LOG:T:\PROJECTS\AKR\FMSS\PHOTOS\PROCESSING\cmd\photothumblogfile.txt
-cp "T:\PROJECTS\AKR\FMSS\PHOTOS\PROCESSING\website scripts\buildings.csv" \\akrgis.nps.gov\inetApps\buildings\data\buildings.csv
-cp "T:\PROJECTS\AKR\FMSS\PHOTOS\PROCESSING\website scripts\photos.json" \\akrgis.nps.gov\inetApps\fmss\photos.json
+copy "T:\PROJECTS\AKR\FMSS\PHOTOS\PROCESSING\website scripts\buildings.csv" \\akrgis.nps.gov\inetApps\buildings\data\buildings.csv
+copy "T:\PROJECTS\AKR\FMSS\PHOTOS\PROCESSING\website scripts\photos.json" \\akrgis.nps.gov\inetApps\fmss\photos.json
 :EOF
