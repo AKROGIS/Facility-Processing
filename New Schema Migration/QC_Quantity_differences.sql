14c1b13ab857f90432b88cbd063cf69169525daa
Regan Sarwas <regan_sarwas@nps.gov>
2020-01-31 14:52:20 -0900

    Update QC_Quantity_differences.sql
    Do not include perimeters or edge lines

--- New Schema Migration/QC_Quantity_differences.sql
+++ New Schema Migration/QC_Quantity_differences.sql
@@ -1,4 +1,4 @@
-SELECT
+    SELECT
       f.PARK, 'Trail' as Type, f.Location, f. description, F.Qty AS FMSS_Qty, round(g.Feet,0) as GIS_Qty, round(F.Qty/g.Feet,2) AS Ratio
     FROM akr_facility2.dbo.FMSSExport AS f
     JOIN (
@@ -8,12 +8,12 @@ SELECT
       FROM
         akr_facility2.gis.TRAILS_LN_evw
       WHERE
-        FACLOCID IS NOT NULL
+        FACLOCID IS NOT NULL AND LINETYPE = 'Center line'
       GROUP BY
         FACLOCID
     ) AS g
     ON g.FACLOCID = f.Location
-WHERE F.Qty/g.Feet < 0.9 OR F.Qty/g.Feet > 1.1
+    WHERE (F.Qty/g.Feet < 0.9 OR F.Qty/g.Feet > 1.1) and Park = 'BELA'
 UNION ALL
     SELECT
       f.PARK, 'Road' as Type, f.Location, f. description, F.Qty AS FMSS_Qty, round(g.Miles,0) as GIS_Qty, round(F.Qty/g.Miles,2) AS Ratio
@@ -25,12 +25,12 @@ UNION ALL
       FROM
         akr_facility2.gis.ROADS_LN_evw
       WHERE
-        FACLOCID IS NOT NULL
+        FACLOCID IS NOT NULL AND LINETYPE = 'Center line'
       GROUP BY
         FACLOCID
     ) AS g
     ON g.FACLOCID = f.Location
-WHERE F.Qty/g.Miles < 0.9 OR F.Qty/g.Miles > 1.1
+    WHERE (F.Qty/g.Miles < 0.9 OR F.Qty/g.Miles > 1.1)
 UNION ALL 
     SELECT
       f.PARK, 'Parking' as Type, f.Location, f. description, F.Qty AS FMSS_Qty, round(g.Qty,0) as GIS_Qty, round(F.Qty/g.Qty,2) AS Ratio
@@ -47,6 +47,6 @@ UNION ALL
         FACLOCID
     ) AS g
     ON g.FACLOCID = f.Location
-  WHERE (F.Qty/g.Qty < 0.9 OR F.Qty/g.Qty > 1.1)
-  AND f.Asset_Code = '1300'
+    WHERE (F.Qty/g.Qty < 0.9 OR F.Qty/g.Qty > 1.1)
+    AND f.Asset_Code = '1300'
 
